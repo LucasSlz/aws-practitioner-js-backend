de5e57c230d57aeaaf4cb18d51af400d0c3f05c5
Lucas Salazar <lucas_salazar@epam.com>
2025-09-23 16:59:03 -0500

    Initial commit

---
+++ test/aws-practitioner-js-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsPractitionerJsBackend from '../lib/aws-practitioner-js-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-practitioner-js-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsPractitionerJsBackend.AwsPractitionerJsBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
